bcbe85e190dc5746b6fb7052e9028bd56f968375
Eduardo Pires <eduardo.c.pires@outlook.com>
2026-04-01 18:36:02 -0300

    feat: add EF Core configs for Event and Experiment entities

---
+++ ExperimentPlatformInfrastructure/Persistence/Configurations/EventConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using ExperimentPlatformDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExperimentPlatformInfrastructure.Persistence.Configurations
+{
+    public class EventConfiguration : IEntityTypeConfiguration<Event>
+    {
+        public void Configure(EntityTypeBuilder<Event> builder)
+        {
+            builder.ToTable("Events");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.UserId);
+
+            builder.Property(x => x.ExperimentId)
+                .HasColumnName("experiment_id");
+
+            //builder.Property(x => x.Variant)
+            //    .HasMaxLength(50)
+            //    .HasColumnName("variant");
+
+            builder.Property(x => x.Type);
+
+            builder.Property(x => x.CreatedAt)
+                .HasColumnName("created_at");
+
+            builder.HasIndex(x => new { x.ExperimentId, x.UserId });
+        }
+    }
+}
---
+++ ExperimentPlatformInfrastructure/Persistence/Configurations/ExperimentConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using ExperimentPlatformDomain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExperimentPlatformInfrastructure.Persistence.Configurations
+{
+    public class ExperimentConfiguration : IEntityTypeConfiguration<Experiment>
+    {
+        public void Configure(EntityTypeBuilder<Experiment> builder)
+        {
+            builder.ToTable("Experiments");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.IsActive);
+
+            builder.HasMany(x => x.Variants)
+                .WithOne()
+                .HasForeignKey("experiment_id")
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
