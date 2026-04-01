be47180aee13f83b6fc8839d42db5cb54927f001
Eduardo Pires <eduardo.c.pires@outlook.com>
2026-04-01 18:24:09 -0300

    feat: add IBackgroundQueue interface and TrackEventHandler class

---
+++ ExperimentPlatformApplication/Abstractions/IBackgroundQueue.cs
@@ -0,0 +1,8 @@
+﻿namespace ExperimentPlatformApplication.Abstractions
+{
+    public interface IBackgroundQueue
+    {
+        void Enqueue(object workItem);
+        Task<object?> DequeueAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ ExperimentPlatformApplication/Events/TrackEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using ExperimentPlatformApplication.Abstractions;
+using ExperimentPlatformDomain.Entities;
+
+namespace ExperimentPlatformApplication.Events
+{
+    public class TrackEventHandler(IBackgroundQueue queue)
+    {
+        private readonly IBackgroundQueue _queue = queue;
+
+        public Task Handle(Guid experimentId, string userId, string type)
+        {
+            var ev = new Event
+            {
+                Id = Guid.NewGuid(),
+                ExperimentId = experimentId,
+                UserId = userId,
+                Type = type,
+                CreatedAt = DateTime.UtcNow
+            };
+
+            _queue.Enqueue(ev);
+
+            return Task.CompletedTask;
+        }
+    }
+}
