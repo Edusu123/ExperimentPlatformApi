742115d6b9a1598d6f3645a9416975190456900e
Eduardo Pires <eduardo.c.pires@outlook.com>
2026-04-01 16:52:44 -0300

    feat: add ExperimentStatus enum and UserId value object

---
+++ ExperimentPlatformDomain/Enums/ExperimentStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace ExperimentPlatformDomain.Enums
+{
+    public enum ExperimentStatus
+    {
+        Draft = 0,
+        Running = 1,
+        Paused = 2,
+        Completed = 3,
+        Archived = 4
+    }
+}
---
+++ ExperimentPlatformDomain/ValueObjects/UserId.cs
@@ -0,0 +1,13 @@
+﻿namespace ExperimentPlatformDomain.ValueObjects
+{
+    public readonly record struct UserId(Guid Value)
+    {
+        public static UserId New() => new(Guid.NewGuid());
+
+        public override string ToString() => Value.ToString();
+
+        public static implicit operator Guid(UserId id) => id.Value;
+
+        public static implicit operator UserId(Guid value) => new(value);
+    }
+}
