2435f46b7a9d64fbd8d1066645487a3d116d8a98
Eduardo Pires <eduardo.c.pires@outlook.com>
2026-04-01 16:23:18 -0300

    feat: database context and context factory

--- ExperimentPlatformInfrastructure/Persistence/ExperimentDbContext.cs
+++ ExperimentPlatformInfrastructure/Persistence/ExperimentDbContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ExperimentPlatformInfrastructure.Persistence
 {
-    public class ExperimentDbContext(DbContextOptions options) : DbContext(options)
+    public class ExperimentDbContext(DbContextOptions<ExperimentDbContext> options) : DbContext(options)
     {
         public DbSet<Experiment> Experiments => Set<Experiment>();
         public DbSet<Event> Events => Set<Event>();
---
+++ ExperimentPlatformInfrastructure/Persistence/ExperimentDbContextFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace ExperimentPlatformInfrastructure.Persistence
+{
+    public class ExperimentDbContextFactory : IDesignTimeDbContextFactory<ExperimentDbContext>
+    {
+        public ExperimentDbContext CreateDbContext(string[] args)
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production";
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddJsonFile(
+                    $"appsettings.{environment}.json",
+                    optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("db");
+
+            var optionsBuilder =
+                new DbContextOptionsBuilder<ExperimentDbContext>();
+
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new ExperimentDbContext(optionsBuilder.Options);
+        }
+    }
+}
