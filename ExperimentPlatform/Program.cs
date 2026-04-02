d267431683fe13fdf33da2982b8ad3663094702a
Eduardo Pires <eduardo.c.pires@outlook.com>
2026-04-02 10:28:08 -0300

    feat: add infrastructure DI extension and update service setup

--- ExperimentPlatform/Program.cs
+++ ExperimentPlatform/Program.cs
@@ -1,3 +1,4 @@
+using ExperimentPlatformInfrastructure;
 using ExperimentPlatformInfrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,11 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<ExperimentDbContext>(
     o => o.UseNpgsql(builder.Configuration.GetConnectionString("db")));
+
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
---
+++ ExperimentPlatformInfrastructure/DependencyInjection.cs
@@ -0,0 +1,27 @@
+﻿using ExperimentPlatformApplication.Abstractions;
+using ExperimentPlatformDomain.Interfaces;
+using ExperimentPlatformInfrastructure.Background;
+using ExperimentPlatformInfrastructure.Persistence;
+using ExperimentPlatformInfrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExperimentPlatformInfrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ExperimentDbContext>(o =>
+                o.UseNpgsql(config.GetConnectionString("db")));
+
+            services.AddScoped<IExperimentRepository, ExperimentRepository>();
+            services.AddScoped<IEventRepository, EventRepository>();
+
+            services.AddSingleton<IBackgroundQueue, InMemoryBackgroundQueue>();
+
+            return services;
+        }
+    }
+}
