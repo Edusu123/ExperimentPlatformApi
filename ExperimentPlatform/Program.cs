085cf591b4bb2c6cbea819db783762b1765789ba
Eduardo Pires <eduardo.c.pires@outlook.com>
2026-04-17 18:50:27 -0300

    config: queue type via settings and register implementation

--- ExperimentPlatform/Program.cs
+++ ExperimentPlatform/Program.cs
@@ -1,5 +1,7 @@
 using ExperimentPlatform.Extensions;
+using ExperimentPlatformApplication.Abstractions;
 using ExperimentPlatformInfrastructure;
+using ExperimentPlatformInfrastructure.Background;
 using ExperimentPlatformInfrastructure.Persistence;
 using ExperimentPlatformWorker;
 using Microsoft.EntityFrameworkCore;
@@ -7,17 +9,39 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
-builder.Services.AddHealthChecks();
-
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddExperimentPlatformBackgroundWorker();
+
+// Configure queue settings
+builder.Services.Configure<QueueSettings>(
+    builder.Configuration.GetSection(QueueSettings.SectionName));
+
+builder.Services.Configure<RabbitMqSettings>(
+    builder.Configuration.GetSection(RabbitMqSettings.SectionName));
+
+var queueSettings = builder.Configuration
+    .GetSection(QueueSettings.SectionName)
+    .Get<QueueSettings>() ?? new QueueSettings();
+
+var queueType = queueSettings.GetQueueType();
+
+// Register only the selected queue implementation
+if (queueType == QueueType.RabbitMQ)
+{
+    builder.Services.AddSingleton<IBackgroundQueue, RabbitMqBackgroundQueue>();
+}
+else
+{
+    builder.Services.AddSingleton<IBackgroundQueue, InMemoryBackgroundQueue>();
+}
+
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
